3be14c6ecfa190a0a959a227515db99d492b59d0
Ismael Ramos <ramosdumas_ismael@hotmail.com>
2025-04-10 23:12:53 -0600

    Update factory logic

--- database/factories/GroupFactory.php
+++ database/factories/GroupFactory.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\ParentGroup>
  */
-class ParentGroupFactory extends Factory
+class GroupFactory extends Factory
 {
     /**
      * Define the model's default state.
@@ -17,7 +17,8 @@ class ParentGroupFactory extends Factory
     public function definition(): array
     {
         return [
-            
+            'name' => $this->faker->unique()->name(),
+            'description' => $this->faker->text(),
         ];
     }
 }
